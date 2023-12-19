b6d9afd05d751c52489a908bd5c16a28d3fa2a60
FlanB <clement.renou@mmibordeaux.com>
2023-12-20 00:41:07 +0100

    ✨ (Camera): add a none functional fpsCamera

--- src/WebGL/Camera.js
+++ src/WebGL/Camera.js
@@ -1,8 +1,23 @@
 import Experience from './Experience.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { CameraHelper, PerspectiveCamera, Vector3 } from 'three'
-import useTransformControls from 'utils/useTransformControls.js'
 
+function setCameraDebugPositionAndTarget(camera) {
+	const debugCameraPosition = JSON.parse(sessionStorage.getItem('debugCameraPosition'))
+	const debugCameraTarget = JSON.parse(sessionStorage.getItem('debugCameraTarget'))
+
+	if (debugCameraPosition) {
+		camera.position.copy(new Vector3(debugCameraPosition.x, debugCameraPosition.y, debugCameraPosition.z))
+	} else {
+		camera.position.copy(this.options.position)
+	}
+
+	if (debugCameraTarget) {
+		camera.lookAt(new Vector3(debugCameraTarget.x, debugCameraTarget.y, debugCameraTarget.z))
+	} else {
+		camera.lookAt(this.options.target)
+	}
+}
 export default class Camera {
 	constructor() {
 		this.experience = new Experience()
@@ -16,11 +31,13 @@ export default class Camera {
 			frustum: { min: 1, max: 100 },
 			position: new Vector3(6, 4, 8),
 			target: new Vector3(0, 0, 0),
-			controlsCamera: { active: false, position: new Vector3(6, 4, 8), target: new Vector3(0, 0, 0) },
+			currentCamera: 'sceneCamera',
 		}
 
 		this.setInstance()
-		if (this.options.controlsCamera.active) this.setControlsCamera()
+		if (this.options.currentCamera === 'controlsCamera') this.setControlsCamera()
+		if (this.options.currentCamera === 'fpsCamera') this.setFpsCamera()
+
 		if (this.debug.active) this.setDebug()
 	}
 
@@ -34,7 +51,7 @@ export default class Camera {
 		this.sceneCamera.position.copy(this.options.position)
 		this.sceneCamera.lookAt(this.options.target)
 		this.sceneCamera.updateMatrixWorld()
-		this.sceneCamera.name = 'camera'
+		this.sceneCamera.name = 'sceneCamera'
 
 		this.instance = this.sceneCamera
 	}
@@ -46,40 +63,45 @@ export default class Camera {
 
 		//Apply saved settings
 		this.controlsCamera.controls.addEventListener('change', () => {
-			sessionStorage.setItem('controlsCameraPosition', JSON.stringify(this.controlsCamera.position))
-			sessionStorage.setItem('controlsCameraTarget', JSON.stringify(this.controlsCamera.controls.target))
+			sessionStorage.setItem('debugCameraPosition', JSON.stringify(this.controlsCamera.position))
+			sessionStorage.setItem('debugCameraTarget', JSON.stringify(this.controlsCamera.controls.target))
 		})
 
-		const controlsCameraPosition = JSON.parse(sessionStorage.getItem('controlsCameraPosition'))
-		const controlsCameraTarget = JSON.parse(sessionStorage.getItem('controlsCameraTarget'))
-		if (controlsCameraPosition) {
-			this.controlsCamera.position.copy(
-				new Vector3(controlsCameraPosition.x, controlsCameraPosition.y, controlsCameraPosition.z),
-			)
-		} else {
-			this.controlsCamera.position.copy(this.options.controlsCamera.position)
-		}
-		if (controlsCameraTarget) {
-			this.controlsCamera.lookAt(new Vector3(controlsCameraTarget.x, controlsCameraTarget.y, controlsCameraTarget.z))
-		} else {
-			this.controlsCamera.lookAt(this.options.controlsCamera.target)
-		}
+		setCameraDebugPositionAndTarget.bind(this)(this.controlsCamera)
 
 		//Helper
-		this.instance.cameraHelper = new CameraHelper(this.instance)
-		this.instance.cameraHelper.name = 'cameraHelper'
-		this.scene.add(this.instance.cameraHelper)
+		if (!this.sceneCamera.cameraHelper) {
+			this.sceneCamera.cameraHelper = new CameraHelper(this.sceneCamera)
+			this.sceneCamera.cameraHelper.name = 'cameraHelper'
+			this.scene.add(this.sceneCamera.cameraHelper)
+		}
 
 		this.instance = this.controlsCamera
 	}
 
-	resetControls() {
-		sessionStorage.removeItem('controlsCameraPosition')
-		sessionStorage.removeItem('controlsCameraTarget')
+	setFpsCamera() {
+		this.fpsCamera = new PerspectiveCamera(50, this.sizes.width / this.sizes.height)
+		this.fpsCamera.name = 'fpsCamera'
 
-		this.controlsCamera.controls.reset()
-		this.controlsCamera.position.copy(this.options.controlsCamera.position)
-		this.controlsCamera.lookAt(this.options.controlsCamera.target)
+		setCameraDebugPositionAndTarget.bind(this)(this.fpsCamera)
+
+		//Helper
+		if (!this.sceneCamera.cameraHelper) {
+			this.sceneCamera.cameraHelper = new CameraHelper(this.sceneCamera)
+			this.sceneCamera.cameraHelper.name = 'cameraHelper'
+			this.scene.add(this.sceneCamera.cameraHelper)
+		}
+
+		this.instance = this.fpsCamera
+	}
+
+	resetDebugPosition() {
+		sessionStorage.removeItem('debugCameraPosition')
+		sessionStorage.removeItem('debugCameraTarget')
+
+		if (this.instance.name === 'sceneCamera') return
+		this.instance.position.copy(this.options.position)
+		this.instance.lookAt(this.options.target)
 	}
 
 	resize() {
@@ -107,27 +129,33 @@ export default class Camera {
 		})
 
 		debugFolder
-			.addBinding(this.options.controlsCamera, 'active', {
-				label: 'Controls camera',
+			.addBlade({
+				view: 'list',
+				label: 'currentCamera',
+				options: [
+					{ text: 'SceneCamera', value: 'sceneCamera' },
+					{ text: 'ControlsCamera', value: 'controlsCamera' },
+					{ text: 'FpsCamera', value: 'fpsCamera' },
+				],
+				value: this.instance.name,
 			})
 			.on('change', ({ value }) => {
-				if (value) {
-					if (this.controlsCamera) {
-						this.instance.cameraHelper.visible = true
-					} else {
-						this.setControlsCamera()
-					}
-					this.instance = this.controlsCamera
-				} else {
-					this.instance = this.sceneCamera
-					this.instance.cameraHelper.visible = false
+				const isSceneCamera = value === 'sceneCamera'
+
+				if (!isSceneCamera) {
+					this[`set${value.charAt(0).toUpperCase() + value.slice(1)}`]()
+					setCameraDebugPositionAndTarget.bind(this)(this[value])
 				}
+
+				this.sceneCamera.cameraHelper.visible = !isSceneCamera
+				if (this.controlsCamera) this.controlsCamera.controls.enabled = value === 'controlsCamera'
+				this.instance = this[value]
 			})
 
 		debugFolder
 			.addButton({
-				title: 'Reset Controls',
+				title: 'Reset debug position',
 			})
-			.on('click', this.resetControls.bind(this))
+			.on('click', this.resetDebugPosition.bind(this))
 	}
 }
