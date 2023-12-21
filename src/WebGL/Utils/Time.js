9139001654d5c70f8e44bb45bd642749d09751ab
FlanB <clement.renou@mmibordeaux.com>
2023-12-21 18:56:31 +0100

    ⚡️ (Time): use performance.now()

--- src/WebGL/Utils/Time.js
+++ src/WebGL/Utils/Time.js
@@ -5,26 +5,21 @@ export default class Time extends EventEmitter {
 		super()
 
 		// Setup
-		this.start = Date.now()
+		this.start = performance.now()
 		this.current = this.start
 		this.elapsed = 0
 		this.delta = 16
 
-		requestAnimationFrame(() => {
-			this.tick()
-		})
+		requestAnimationFrame(this.tick.bind(this))
 	}
 
-	tick() {
-		const currentTime = Date.now()
+	tick(currentTime) {
 		this.delta = currentTime - this.current
 		this.current = currentTime
 		this.elapsed = this.current - this.start
 
 		this.trigger('tick')
 
-		requestAnimationFrame(() => {
-			this.tick()
-		})
+		requestAnimationFrame(this.tick.bind(this))
 	}
 }
