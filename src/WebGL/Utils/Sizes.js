31a17be5dc85e6f28786d534c7aca0cdf33b9600
FlanB <clement.renou@mmibordeaux.com>
2023-12-26 19:17:38 +0100

    ♻️ (isMobile): put in Sizes.js

--- src/WebGL/Utils/Sizes.js
+++ src/WebGL/Utils/Sizes.js
@@ -8,14 +8,21 @@ export default class Sizes extends EventEmitter {
 		this.width = window.innerWidth
 		this.height = window.innerHeight
 		this.pixelRatio = Math.min(devicePixelRatio, 2)
+		this.isMobile = this.#checkIfMobile()
 
 		// Resize event
 		addEventListener('resize', () => {
 			this.width = window.innerWidth
 			this.height = window.innerHeight
 			this.pixelRatio = Math.min(devicePixelRatio, 2)
+			this.isMobile = this.#checkIfMobile()
 
 			this.trigger('resize')
 		})
+	}
+	#checkIfMobile() {
+		const isMobileUa = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+		const isMobileScreen = this.width < 1024
+		return isMobileUa || isMobileScreen
 	}
 }
